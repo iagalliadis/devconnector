3d11a6c880e239950dd82b25c7b3c863fa6cd423
iagalliadis <johnalias25@gmail.com>
2019-09-09 01:01:46 +0300

    Create jWT payload and sign token for user

--- config/keys.js
+++ config/keys.js
@@ -1,3 +1,4 @@
 module.exports = {
-    mongoURI:'mongodb+srv://john:john@cluster0-24htr.mongodb.net/test?retryWrites=true&w=majority'
+    mongoURI:'mongodb+srv://john:john@cluster0-24htr.mongodb.net/test?retryWrites=true&w=majority',
+    secretOrKey: 'secret'
 }; 
