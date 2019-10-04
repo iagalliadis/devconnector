91a477e3074e05fe6b1eec1188367110f1f7727d
iagalliadis <johnalias25@gmail.com>
2019-10-04 10:17:05 +0300

    stopped because 400 bad request

--- server.js
+++ server.js
@@ -1,39 +1,38 @@
-const express = require('express')
-const mongoose = require('mongoose')
+const express = require('express');
+const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
-
+const passport = require('passport');
 
 const users = require('./routes/api/users');
-const posts = require('./routes/api/posts');
 const profile = require('./routes/api/profile');
-const passport = require('passport');
+const posts = require('./routes/api/posts');
 
 const app = express();
 
-//body parser middleware
-app.use(bodyParser.urlencoded({extended:false}));
+// Body parser middleware
+app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
-//DB config
+// DB Config
 const db = require('./config/keys').mongoURI;
-//connect to mongoDB
-mongoose
-    .connect(db)
-    .then(() => console.log('MongoDB connected'))
-    .catch(err => console.log(err))
 
-//Passport middleware
+// Connect to MongoDB
+mongoose
+  .connect(db)
+  .then(() => console.log('MongoDB Connected'))
+  .catch(err => console.log(err));
+
+// Passport middleware
 app.use(passport.initialize());
 
-//Passport config
+// Passport Config
 require('./config/passport')(passport);
 
-
-//Use routes  
-app.use('/api/users',users);
-app.use('/api/posts',posts);
-app.use('/api/profile',profile);
+// Use Routes
+app.use('/api/users', users);
+app.use('/api/profile', profile);
+app.use('/api/posts', posts);
 
 const port = process.env.PORT || 5000;
 
-app.listen(port, ()=>console.log(`Server running on ${port}`));
+app.listen(port, () => console.log(`Server running on port ${port}`));
